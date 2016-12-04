7556e8ddd0d1f0c95c24f45512f77ba040d0f2e1
Harry John <hajohn@microsoft.com>
2016-12-04 17:03:02 +0530

    Adding webpack config, tslint, and other dev setup

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+let x = 24;
+
+let y: string = 'blah' + 'blah' + 'blah' + 'blah';
+
+@NgModule({
+
+})
+export class A {
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+var webpack = require('webpack');
+var PACKAGE = require('./package.json');
+var path = require('path');
+var yargs = require('yargs');
+
+var libraryName = PACKAGE.name,
+    plugins = [],
+    outputFile;
+
+if (yargs.argv.p) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+    outputFile = libraryName + '.min.js';
+} else {
+    outputFile = libraryName + '.js';
+}
+
+
+var config = {
+    entry: path.join(__dirname, 'src', 'main.ts'),
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, 'lib'),
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.ts$/,
+                loader: 'tslint',
+                include: /src/,
+            },
+        ],
+        loaders: [
+            {
+                test: /\.ts$/,
+                include: /src/,
+                loader: 'ts',
+                query: {
+                    silent: true
+                }
+            },
+        ],
+    },
+    resolve: {
+        root: path.resolve('./src'),
+        extensions: ['', '.js', '.ts']
+    },
+    plugins: plugins,
+
+    // Individual Plugin Options
+    tslint: {
+        emitErrors: true,
+        failOnHint: true
+    },
+
+    // Externals
+    externals: {
+        '@angular/core': '@angular/core',
+    }
+};
+
+module.exports = config;
